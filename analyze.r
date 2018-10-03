60c08d90872cee1fc8ec9a0a9ade31dbe13e5994
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2018-10-03 19:52:33 +0200

    Clean up

--- analyze.r
+++ analyze.r
@@ -64,13 +64,11 @@ load_data <- function(paths, transpose=FALSE) {
   for (count in counts)
     genes <- union(genes, rownames(count))
 
-  # print(str(genes))
   section <- c()
   coord <- c()
   m <- matrix(0, nrow=length(genes), ncol=0)
   rownames(m) <- genes
   idx <- 1
-  # print(str(m))
   for (count in counts) {
     n <- matrix(0, nrow=length(genes), ncol=ncol(count))
     rownames(n) <- genes
@@ -79,20 +77,13 @@ load_data <- function(paths, transpose=FALSE) {
     m <- cbind(m, n)
     section <- c(section, rep(idx, ncol(count)))
     coord <- c(coord, colnames(count))
-    # print(idx)
-    # print(str(m))
     idx <- idx + 1
   }
-
-  # print(str(section))
-  # print(table(section))
-  # print(str(coord))
 
   coord <- matrix(unlist(lapply(strsplit(coord, "x"), as.numeric)), ncol=2, byrow=TRUE)
 
   colData <- list(section=section, x=coord[,1], y=coord[,2])
 
-  # print(m[1:10,1:10])
   SummarizedExperiment(assays=list(counts=m), colData=colData)
 }
 
@@ -126,7 +117,6 @@ analyze <- function(expr, K=2, num_genes=100) {
 
   zinb <- zinbwave(expr, K=K, epsilon=1000)
 
-  # print(str(zinb))
   list(expr=expr, zinb=zinb)
 }
 
@@ -135,7 +125,6 @@ visualize_it <- function(expr, zinb, output_prefix=NULL) {
   sections <- unique(sort(expr$section))
 
   W <- reducedDim(zinb)
-  # print(str(W))
   if (!is.null(output_prefix)) {
     write.table(W, file=paste0(output_prefix, "ZINB-WaVE-output.tsv"), sep="\t", quote=FALSE)
     write.table(colData(expr), file=paste0(output_prefix, "ZINB-WaVE-colData.tsv"), sep="\t", quote=FALSE)
@@ -151,21 +140,14 @@ visualize_it <- function(expr, zinb, output_prefix=NULL) {
     ggplot(aes(W1, W2, colour=section)) + geom_point() + 
     scale_color_brewer(type = "qual", palette = "Set1") +
     theme_classic()
-    #scale_color_brewer(type = "qual", palette = "Set1") + theme_classic()
-  # data.frame(W, bio=colData(expr)$Biological_Condition,
-  #          coverage=colData(expr)$Coverage_Type) %>%
-  #   ggplot(aes(W1, W2, colour=bio, shape=coverage)) + geom_point() + 
-  #   scale_color_brewer(type = "qual", palette = "Set1") + theme_classic()
 
   n <- ceiling(sqrt(length(sections)))
   pdf(paste0(output_prefix, "ZINB-WaVE-visual.pdf"), width=n*6, height=n*6)
-  # print(str(coords))
   for (col_idx in 1:ncol(W)) {
     par(mfrow=c(n, n))
     for (section_idx in sections) {
       these <- expr$section==section_idx
       w <- W[these,col_idx]
-      # print(str(w))
       visualize(w, coords=coords[these,])
     }
   }
